5f627dc49e74cd16f7eae2aab7a65799a818eded
rockydd <rockydd@gmail.com>
2011-10-15 07:11:01 -0400

    changed the way of create activity, use animation to show the process

--- fanfan/public/javascripts/application.js
+++ fanfan/public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function moveAnimate(element, newParent){
+  var oldOffset = element.offset();
+  element.appendTo(newParent);
+  var newOffset = element.offset();
+
+  var temp = element.clone().appendTo('body');
+  temp    .css('position', 'absolute')
+    .css('left', oldOffset.left)
+    .css('top', oldOffset.top)
+    .css('zIndex', 1000);
+  element.hide();
+  temp.animate( {'top': newOffset.top, 'left':newOffset.left}, 'slow', function(){
+      element.show();
+      temp.remove();
+      });
+}
