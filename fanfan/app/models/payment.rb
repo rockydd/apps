81289f982a27df7053d4e15e17e4d3efbc457699
shawn zhang <shawn@shawn-dev.(none)>
2011-11-10 21:52:28 +0800

    remove exception handle

--- fanfan/app/models/payment.rb
+++ fanfan/app/models/payment.rb
@@ -1,4 +1,14 @@
+
+# CREATE TABLE "payments" (
+# "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+# "activity_id" integer, 
+# "user_id" integer, 
+# "amount" decimal, 
+# "final" boolean, 
+# "confirmed" boolean
+# );
 class Payment < ActiveRecord::Base
   belongs_to :activity
   belongs_to :user
+  validates_uniqueness_of :user_id, :scope => :activity_id, :message => "Duplicate user found"
 end
