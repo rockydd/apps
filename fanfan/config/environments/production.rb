e1e228bddab63534f748e96dcb45d7f53d0ed215
rockydd <rockydd@gmail.com>
2011-12-29 12:26:44 -0500

    mail setting and i18n

--- fanfan/config/environments/production.rb
+++ fanfan/config/environments/production.rb
@@ -46,4 +46,21 @@ Fanfan::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  # mail:
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  #  sendmail
+  #config.action_mailer.delivery_method = :sendmail
+  #  smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'fanfan.bz',
+  :user_name            => 'fanfan.dev',
+  :password             => 'atmosisgreat',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
 end
