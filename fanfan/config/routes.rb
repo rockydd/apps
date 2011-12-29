5da4a60f11d9efc770b4f5a03b296949c78a2fa1
rockydd <rockydd@gmail.com>
2011-12-29 01:32:36 -0500

    added beta tag. autocomplete to 1, root to activities

--- fanfan/config/routes.rb
+++ fanfan/config/routes.rb
@@ -75,7 +75,7 @@ Fanfan::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "sessions#new"
+  root :to => "activities#index"
 
   # See how all your routes lay out with "rake routes"
 
