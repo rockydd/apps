55a82c07ca02f76d63ecec90b254c197ca74325c
Mario Tao <Tao.Huawei@emc.com>
2012-10-31 18:35:44 +0800

    add rspec spork guard

---
+++ fanfan/spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe User do
+  it "has a valid factory" do
+    FactoryGirl.create(:user).should be_valid
+  end
+  
+  it "is invalid without username" do
+    FactoryGirl.build(:user, username:nil).should_not be_valid
+  end
+
+  it "is invalid without username2" do
+    FactoryGirl.build(:user, username:nil).should_not be_valid    
+  end
+
+  it "is invalid without username3" do
+    FactoryGirl.build(:user, username:nil).should_not be_valid    
+  end
+end
